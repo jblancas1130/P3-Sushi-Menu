57bb38363901dded8b81f069d0d0b4aa5e4a1daf
jessica <jessica_blanc30@hotmail.com>
2013-05-30 11:19:46 -0700

    Final Commit
    This is the final menu

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,16 @@ $(document).ready(function() {
   
 	  $( "#iPicture" ).iPicture();
 });	
+
+$(function () {
+    $.scrollUp({
+        scrollName: 'scrollUp', 
+        topDistance: '350', 
+        topSpeed: 300, 
+        animation: 'fade', 
+        animationInSpeed: 200, 
+        animationOutSpeed: 200, 
+        activeOverlay: false, 
+    });
+
+});
